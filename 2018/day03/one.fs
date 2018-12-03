ce670f340eb14f6522cca91f92f54a3d6c9354db
cheesejake <34197255+cheesejake@users.noreply.github.com>
2018-12-03 16:45:25 +0000

    still bugs to squach

--- 2018/day03/one.fs
+++ 2018/day03/one.fs
@@ -2,8 +2,7 @@
 \ gforth-fast -d 100K input one.fs
 \             ^^^^^^^ data stack size
 
-variable @n \ number of claims
-create map 1000 1000 * chars allot
+create map 1010 1010 * chars allot
 
 : organize-stack ( ... x l t w h -- ... w+l+1 l h+t+1 t )
    >r >r >r >r drop               ( ... ) ( R: h w t l )
@@ -16,12 +15,34 @@ create map 1000 1000 * chars allot
 
 : mapcount ( id1 l1 t1 w1 h1 id2 l2 t2 w2 h2 ... idN lN tN wN hN -- )
    begin
-       organize-stack
-       do do
-       loop loop
-   depth 0 = until
+      organize-stack
+      do
+         2dup do
+            map i 1010 * j + chars + c@
+            1 + dup 120 > if drop 2 then
+            map i 1010 * j + chars + c!
+         loop
+      loop 2drop
+      ." after mapcount loops" space .s cr
+   depth 4 < until
 ;
 
-depth 5 / @n !
-\ mapcount
-\ cr bye
+: zeromap ( -- )
+   1010 0 do 1010 0 do
+      0 map i 1010 * j + chars + c!
+   loop loop
+;
+
+: counttwos ( -- n )
+   0                     \ starting count
+   1010 0 do 1010 0 do
+      map i 1010 * j + chars + c@
+      dup 1 > if swap 1 + swap i . j . then
+      drop
+   loop loop
+;
+
+zeromap ." zeromap ok" space cr
+mapcount ." mapcount ok" space cr
+counttwos
+. cr bye
