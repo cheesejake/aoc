f32b425351afa64124082d0210191943c99b4934
cheesejake <34197255+cheesejake@users.noreply.github.com>
2018-12-03 15:44:06 +0000

    organization ok

--- 2018/day03/one.fs
+++ 2018/day03/one.fs
@@ -1,14 +1,17 @@
 \ call with
-\ gforth-fast input one.fs
+\ gforth-fast -d 100K input one.fs
+\             ^^^^^^^ data stack size
 
 variable @n \ number of claims
 create map 1000 1000 * chars allot
 
-: organize-stack ( x l t w h -- w+l+1 l t+h+1 t )
-   >r swap                    ( x l w t ) ( R: h )
-   dup >r + swap >r >r        ( x l w ) ( R: t t+h )
-   over + 1 + swap            ( x w+l+1 l ) ( R: t t+h )
-   >r >r drop r> r> r> 1 + r> ( w+l l t+h+1 t )
+: organize-stack ( ... x l t w h -- ... w+l+1 l h+t+1 t )
+   >r >r >r >r drop               ( ... ) ( R: h w t l )
+   r> r> r>                       ( ... l t w ) ( R: h )
+   swap >r                        ( ... l w ) ( R: h t )
+   over 1 + + swap                ( ... w+l+1 l ) ( R: h t )
+   r> dup r>                      ( ... w+l+1 l t t h )
+   1 + + swap                     ( ... w+l+1 l t+h+1 t )
 ;
 
 : mapcount ( id1 l1 t1 w1 h1 id2 l2 t2 w2 h2 ... idN lN tN wN hN -- )
