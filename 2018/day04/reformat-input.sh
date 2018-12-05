8751106220e6819596fa3f486089e6a4b17a8d5c
cheesejake <34197255+cheesejake@users.noreply.github.com>
2018-12-05 16:37:18 +0000

    sort from Forth

--- 2018/day04/reformat-input.sh
+++ 2018/day04/reformat-input.sh
@@ -4,3 +4,5 @@ sed -E -e 's/^\[(.*)-(.*)-(.*) (.*):(.*)\] (.*)$/\1 \2 \3 \4 \5 \6/' <input.txt 
 sed -E -e 's/^(.*) Guard \#([0123456789]*) begins shift$/\1 \2 1 2/' <input2.txt >input3.txt
 sed -E -e 's/^(.*) wakes up$/\1 1 1/' <input3.txt >input4.txt
 sed -E -e 's/^(.*) falls asleep$/\1 0 1/' <input4.txt >justnumbers-input.txt
+
+# I decided to leave the file unsorted
