41dba8fc2424488c37b8b85243f90bc84ee7c701
cheesejake <34197255+cheesejake@users.noreply.github.com>
2018-12-05 11:50:37 +0000

    day03 part 1 done

--- 2018/day03/one.fs
+++ 2018/day03/one.fs
@@ -1,30 +1,33 @@
 \ call with
-\ gforth-fast -d 100K input one.fs
+\ gforth-fast -d 100k .justnumbers-input.txt one.fs
 \             ^^^^^^^ data stack size
 
+variable areas
 create map 1010 1010 * chars allot
 
-: organize-stack ( ... x l t w h -- ... w+l+1 l h+t+1 t )
+: organize-stack ( ... x l t w h -- ... w+l l h+t t )
    >r >r >r >r drop               ( ... ) ( R: h w t l )
    r> r> r>                       ( ... l t w ) ( R: h )
    swap >r                        ( ... l w ) ( R: h t )
-   over 1 + + swap                ( ... w+l+1 l ) ( R: h t )
-   r> dup r>                      ( ... w+l+1 l t t h )
-   1 + + swap                     ( ... w+l+1 l t+h+1 t )
+   over + swap                    ( ... w+l l ) ( R: h t )
+   r> dup r>                      ( ... w+l l t t h )
+   + swap                         ( ... w+l l t+h t )
 ;
 
-: mapcount ( id1 l1 t1 w1 h1 id2 l2 t2 w2 h2 ... idN lN tN wN hN -- )
+: mapareas ( id1 l1 t1 w1 h1 id2 l2 t2 w2 h2 ... idN lN tN wN hN -- )
    begin
+\      ." before mapareas loops" space .s cr
       organize-stack
       do
          2dup do
             map i 1010 * j + chars + c@
+\            ." marking" space i . j . cr
             1 + dup 120 > if drop 2 then
             map i 1010 * j + chars + c!
          loop
       loop 2drop
-      ." after mapcount loops" space .s cr
-   depth 4 < until
+\      ." after mapareas loops" space .s cr
+   depth 0 = until
 ;
 
 : zeromap ( -- )
@@ -33,16 +36,29 @@ create map 1010 1010 * chars allot
    loop loop
 ;
 
+\ show the n by n top left corner of the map
+: showmap ( n -- )
+   ." ==> showmap start" space .s cr
+   dup 0 do
+      dup 0 do
+          map i 1010 * j + chars + c@ .
+      loop
+      cr
+   loop
+   ." ==> showmap finished" space .s cr
+;
+
 : counttwos ( -- n )
    0                     \ starting count
    1010 0 do 1010 0 do
       map i 1010 * j + chars + c@
-      dup 1 > if swap 1 + swap i . j . then
+      dup 1 > if swap 1 + swap then
       drop
    loop loop
 ;
 
 zeromap ." zeromap ok" space cr
-mapcount ." mapcount ok" space cr
+mapareas ." mapareas ok" space cr
+\ 8 showmap
 counttwos
 . cr bye
