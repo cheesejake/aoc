f1babb3a97e1ddd6ef8d198f8a3e3307c242e75a
cheesejake <34197255+cheesejake@users.noreply.github.com>
2019-12-01 13:14:25 +0000

    added a few comments

--- 2019/day01-1.c
+++ 2019/day01-1.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// assume well-formed input, in range values, or that
+// everything 'works'
+
 int main(void) {
     int total = 0;
-    char buf[100];
+    char buf[100];                     // more than enough space
     while (fgets(buf, sizeof buf, stdin)) {
-        int mass = strtol(buf, 0, 10);
-        total += mass / 3 - 2;
+        int mass = strtol(buf, 0, 10); // error checking missing
+        total += mass / 3 - 2;         // integer (truncating) division
     }
     printf("fuel requirements for all modules: %d\n", total);
+    return 0;
 }
--- 2019/day01-2.c
+++ 2019/day01-2.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// assume well-formed input, in range values, or that
+// everything 'works'
+
 int fuel4mass(int mass) {
     if (mass < 3) return 0;
-    int tmp = mass / 3 - 2;
+    int tmp = mass / 3 - 2;            // integer (truncating) division
     if (tmp <= 0) return 0;
-    return tmp + fuel4mass(tmp);
+    return tmp + fuel4mass(tmp);       // recurse
 }
 
 int main(void) {
     int total = 0;
-    char buf[100];
+    char buf[100];                     // more than enough space
     while (fgets(buf, sizeof buf, stdin)) {
-        int mass = strtol(buf, 0, 10);
-        total += fuel4mass(mass);
+        int mass = strtol(buf, 0, 10); // missing error checking
+        total += fuel4mass(mass);      // assume no overflow
     }
     printf("fuel requirements for all modules and fuel: %d\n", total);
+    return 9;
 }
