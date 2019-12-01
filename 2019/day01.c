ce817adffd614f47473268000403ba51053f6f65
cheesejake <34197255+cheesejake@users.noreply.github.com>
2019-12-01 14:34:17 +0000

    restructure part 1 and 2 dymanics

---
+++ 2019/day01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// assume everything 'works'
+// ie: well-formed input, values in range, no overflows
+
+int fuel(int mass) {
+    if (mass < 9) return 0;
+    int tmp = mass / 3 - 2;
+    return tmp + fuel(tmp);            // recurse
+}
+
+int main(void) {
+    int mass, total1 = 0, total2 = 0;
+    while (scanf("%d", &mass) == 1) {
+        total1 += mass / 3 - 2;         // integer (truncating) division
+        total2 += fuel(mass);
+    }
+    printf("fuel requirements for all modules: %d\n", total1);
+    printf("fuel requirements for all modules and fuel: %d\n", total2);
+    return 0;
+}
