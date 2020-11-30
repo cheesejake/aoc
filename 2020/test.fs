2f23a0d741ff417451f4ade6f77f446d6e21883e
cheesejake <34197255+cheesejake@users.noreply.github.com>
2020-11-30 18:12:10 +0000

    use require rether than needs, use dot-paren rather than dot-quote

--- 2020/test.fs
+++ 2020/test.fs
@@ -1,6 +1,6 @@
 \ sum all the numbers in each line of input.txt       2020-11-30
 
-needs simplified-file.fs
+require simplified-file.fs
 
 : work ( fname -- sum )
 open-input 0 begin
@@ -9,4 +9,4 @@ open-input 0 begin
 drop swap close-input ;
 
 s" input.txt" work
-." result: " . cr bye
+.( result: ) . cr bye
