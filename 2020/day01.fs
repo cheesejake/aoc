18f330fafecc6907351ebb30cd89bfe0fb892742
cheesejake <34197255+cheesejake@users.noreply.github.com>
2020-12-01 10:42:39 +0000

    cleanup

--- 2020/day01.fs
+++ 2020/day01.fs
@@ -10,9 +10,14 @@ r/o open-file abort" open failed" ;
 close-file abort" close failed" ;
 
 : input>pad ( fid -- n f ; pad changed )
+\ According to Forth Standard, size of the scratch area whose
+\ address is returned by PAD shall be at least 84 characters.
+\ reads up to 80 characters from file to pad
+\ stack holds quantity read and flag (0 means no input)
 pad 80 rot read-line abort" read failed" ;
 
 : pad>number ( n -- number )
+\ converts the first n digits in pad to number
 0 0 rot pad swap >number drop drop d>s ;
 
 \ leave number of elements read on TOS
