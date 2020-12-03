0d149754f8039edb0a45f74448a64a560c888827
cheesejake <34197255+cheesejake@users.noreply.github.com>
2020-12-03 09:55:02 +0000

    part2, answer too low

--- 2020/day03.fs
+++ 2020/day03.fs
@@ -33,15 +33,31 @@ variable width                      \ effective width from input
 
 \ Part1:
 : part1 ( nlines -- ntrees )
-0 0 ( ntrees col ) rot 0 do                   ( ntrees col )
+   0 0 ( ntrees col ) rot 0 do                ( ntrees col )
    3 + dup width @ mod           ( ntrees col+3 col3%width )
    i 1+ maxcolumns *
    mapmod[] + + c@            ( ntrees col+3 ch )
    '#' = if swap 1+ swap then
    loop drop ;
 
+\ slope part1 with varying slope
+: slope ( deltacols deltarows nlines -- ntrees )
+   0 0 ( ntrees col ) rot 0 do                ( c r ntrees col )
+   3 pick + dup width @ mod      ( c r ntrees col+3 col3%width )
+   i 1+ maxcolumns *
+   mapmod[] + + c@            ( c r ntrees col+3 ch )
+   '#' = if swap 1+ swap then ( c r ntrees col+3 )
+   2 pick +loop drop swap drop swap drop ;
+
 \ Part2:
-: part2 ( nlines -- 0 ) drop 0 ;
+: part2 ( nlines -- prod )
+   1 swap                                ( 1 nl )
+   dup 1 1 rot slope rot * swap          ( t*1 nl )
+   dup 3 1 rot slope rot * swap          ( P*t nl )
+   dup 5 1 rot slope rot * swap          ( P*t nl )
+   dup 7 1 rot slope rot * swap          ( P*t nl )
+   dup 1 2 rot slope rot * swap          ( P*t nl )
+   drop ;
 
 : read-input ( -- n ) \ number of lines read
 next-arg open-input 0                 ( fid 0 )
