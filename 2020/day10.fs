710db05fb36d9b85cfb04cac8a14158fcb08c9ad
cheesejake <34197255+cheesejake@users.noreply.github.com>
2020-12-10 11:50:37 +0000

    day 10, part 2 -- tribonacci numbers

--- 2020/day10.fs
+++ 2020/day10.fs
@@ -48,28 +48,47 @@ next-arg r/o open-file abort" open error" to fid
 1 to n 0 adapters ! \ add 0 to the array
 file>array bubble-sort
 \ add device joltage to array
-n 1- cells adapters + @ 3 +   n cells adapters + !   n 1+ to n ;
+n 1- nth 3 +   n cells adapters + !   n 1+ to n ;
 
 0 value count1
+0 value count2
 0 value count3
 
-: solve1 ( -- ; count1 and count3 updated )
+: solve1 ( -- ; count1 and count3 updated ) \ and count2
 n 1 do i nth i 1- nth -
    dup 1 = if count1 1+ to count1 then
-       3 = if count3 1+ to count3 then loop
+   dup 3 = if count3 1+ to count3 then
+       2 = if count3 1+ to count3 then loop
 ;
 
-: solve2 ( row col -- )
-;
+: fact ( n1 -- n2 )
+dup 2 < if drop 1 exit then dup 1- recurse * ;
+
+: tribonacci ( n -- tribonacci )
+dup 0 = if drop 1 exit then
+dup 1 = if        exit then
+dup 2 = if        exit then
+dup dup 1- recurse rot 2 - recurse rot 3 - recurse + + ;
+
+: seq1's ( index -- index3 n )
+0 begin
+   over dup 1+ nth swap nth - 1 = while
+   1+ swap 1+ swap repeat
+swap 1+ swap ;
+
+1 value product
+: solve2 ( -- )
+1 0 begin seq1's tribonacci rot * swap dup n = until
+drop product * to product ;
 
 : work ( -- )
-solve1 ;
+solve1 solve2 ;
 
 : finish ( -- )
 fid close-file abort" close error" ;
 
 : report ( -- )
 ." Day 10, part 1: " count1 count3 * . cr
-." Day 10, part 2: " -1 . cr ;
+." Day 10, part 2: " product . cr ;
 
 setup work finish report BYE
