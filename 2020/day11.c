2878199d24384ff536f875752f44f127c5cdb085
cheesejake <34197255+cheesejake@users.noreply.github.com>
2020-12-11 08:30:31 +0000

    day 11 in C

---
+++ 2020/day11.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void evolve(char (*dst)[120], char (*src)[120], int n) {
+    for (int row = 0; row < n; row++) {
+        for (int col = 0; src[row][col] != 0; col++) {
+            dst[row][col] = src[row][col];
+            if (src[row][col] == 'L') {
+                int neighbours = 0;
+                if ((row > 0)     && (col > 0)     && (src[row-1][col-1] == '#')) ++neighbours;
+                if ((row > 0)                      && (src[row-1][col] == '#'))   ++neighbours;
+                if ((row > 0)     && (col < n - 1) && (src[row-1][col+1] == '#')) ++neighbours;
+                if (                 (col > 0)     && (src[row][col-1] == '#'))   ++neighbours;
+                if (                 (col < n - 1) && (src[row][col+1] == '#'))   ++neighbours;
+                if ((row < n - 1) && (col > 0)     && (src[row+1][col-1] == '#')) ++neighbours;
+                if ((row < n - 1)                  && (src[row+1][col] == '#'))   ++neighbours;
+                if ((row < n - 1) && (col < n - 1) && (src[row+1][col+1] == '#')) ++neighbours;
+                if (neighbours == 0) dst[row][col] = '#';
+            }
+            if (src[row][col] == '#') {
+                int neighbours = 0;
+                if ((row > 0)     && (col > 0)     && (src[row-1][col-1] == '#')) ++neighbours;
+                if ((row > 0)                      && (src[row-1][col] == '#'))   ++neighbours;
+                if ((row > 0)     && (col < n - 1) && (src[row-1][col+1] == '#')) ++neighbours;
+                if (                 (col > 0)     && (src[row][col-1] == '#'))   ++neighbours;
+                if (                 (col < n - 1) && (src[row][col+1] == '#'))   ++neighbours;
+                if ((row < n - 1) && (col > 0)     && (src[row+1][col-1] == '#')) ++neighbours;
+                if ((row < n - 1)                  && (src[row+1][col] == '#'))   ++neighbours;
+                if ((row < n - 1) && (col < n - 1) && (src[row+1][col+1] == '#')) ++neighbours;
+                if (neighbours >= 4) dst[row][col] = 'L';
+            }
+        }
+    }
+}
+
+int equal(char (*a)[120], char (*b)[120], int n) {
+    for (int k = 0; k < n; k++) {
+        if (strcmp(a[k], b[k])) return 0;
+    }
+    return 1;
+}
+
+int occupied(char (*x)[120], int n) {
+    int count = 0;
+    for (int k = 0; k < n; k++) {
+        char *p = x[k];
+        while (*p) {
+            count += (*p == '#');
+            p++;
+        }
+    }
+    return count;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "syntax: ./a.out <filename>\n");
+        exit(EXIT_FAILURE);
+    }
+    char data[2][200][120] = {0};
+    int n = 0;
+    FILE *fid = fopen(argv[1], "r");
+    char buf[500];
+    while (fgets(buf, sizeof buf, fid)) {
+        buf[strcspn(buf, "\r\n")] = 0;
+        strcpy(data[0][n], buf);
+        strcpy(data[1][n], buf);
+        n++;
+    }
+    fclose(fid);
+
+    int generation = 0;
+    do {
+        evolve(data[(generation + 1) % 2], data[generation % 2], n);
+        generation++;
+    } while (!equal(data[0], data[1], n));
+    printf("day 11, part1: %d occupied seats.\n", occupied(data[0], n));
+}
